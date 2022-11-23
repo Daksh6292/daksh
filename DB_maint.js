b32ac5373dd778f5a73d61424e3b8cfc9c92f523
Daksh6292 <118925406+Daksh6292@users.noreply.github.com>
2022-11-23 23:02:54 +0530

    Add files via upload

---
+++ DB_maint.js
@@ -0,0 +1,85 @@
+function myf() {
+    var data = new XMLHttpRequest();
+    data.open("GET", "https://script.google.com/macros/s/AKfycbznQ4VW3F9_dMyQ94_8OK3HY48ZYZ8Z489Jk4hUiyWHqFyc1c7GbujOCQrmFn5uTEeB9A/exec", true);
+    data.send();
+    data.onload = function my() {
+      var x = JSON.parse(data.responseText);
+      var eq = document.getElementsByName("EQUIPMENT_ID")[0].value;
+      var session = document.getElementsByName("MAINTENANCE_TYPE")[0].value;
+      if (session == "MONTHLY") {
+        document.getElementsByName("DONE_DATE")[0].value = x[14][eq];
+        $("#nextBtn").click();
+        if (x[0][eq].toUpperCase() == "CHECKED") {
+          document.getElementsByName("CHK_SELECTOR_SWITCH_LOCAL_REMOTE")[0].checked = true;
+        } else {
+          document.getElementsByName("CHK_SELECTOR_SWITCH_LOCAL_REMOTE")[1].checked = true;
+        };
+        if (x[1][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_INCOM_VOLT_THREE_PHASE")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_INCOM_VOLT_THREE_PHASE")[1].checked = true;
+          };
+          if (x[2][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_CONTROL_SUPPLY")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_CONTROL_SUPPLY")[1].checked = true;
+          };
+          if (x[3][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_ACB_MCCB_TRIPPING_OR_FAULT_MESSAGE")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_ACB_MCCB_TRIPPING_OR_FAULT_MESSAGE")[1].checked = true;
+          };
+          if (x[4][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_RELAY_STATUS")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_RELAY_STATUS")[1].checked = true;
+          };
+          if (x[5][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_ABNORMAL_NOISE")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_ABNORMAL_NOISE")[1].checked = true;
+          };
+          if (x[6][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("BURNING_SMELL_CHECK")[0].checked = true;
+          } else {
+            document.getElementsByName("BURNING_SMELL_CHECK")[1].checked = true;
+          };
+          if (x[7][eq].toUpperCase() == "DONE") {
+            document.getElementsByName("KEEP_SERVICE_ZONE_CLEAN_LIT_VENTILATED")[0].checked = true;
+          } else {
+            document.getElementsByName("KEEP_SERVICE_ZONE_CLEAN_LIT_VENTILATED")[1].checked = true;
+          };
+          if (x[8][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_DB_FOR_PHYSICAL_MECHANICAL_DAMAGE")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_DB_FOR_PHYSICAL_MECHANICAL_DAMAGE")[1].checked = true;
+          };
+          if (x[9][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("INDICATION_LAMP_CHK")[0].checked = true;
+          } else {
+            document.getElementsByName("INDICATION_LAMP_CHK")[1].checked = true;
+          };
+          if (x[10][eq].toUpperCase() == "DONE") {
+            document.getElementsByName("CLN_OUTSIDE_PANEL")[0].checked = true;
+          } else {
+            document.getElementsByName("CLN_OUTSIDE_PANEL")[1].checked = true;
+          };
+          if (x[11][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_FAN_THERMOSTATE_OPERATION")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_FAN_THERMOSTATE_OPERATION")[1].checked = true;
+          };
+          if (x[12][eq].toUpperCase() == "CHECKED") {
+            document.getElementsByName("CHK_MEASURING_INSTRUMENT_ENERGY_METER")[0].checked = true;
+          } else {
+            document.getElementsByName("CHK_MEASURING_INSTRUMENT_ENERGY_METER")[1].checked = true;
+          };
+        document.getElementsByName("REMARKS")[0].value = x[13][eq];
+  
+  
+      } else if (session == "Mquartly") {
+        
+      }
+    }
+  };
+  myf();
---
+++ fcu_maint.js
@@ -0,0 +1,104 @@
+function myf(){        
+    var data = new XMLHttpRequest();        
+    data.open("GET", "https://script.google.com/macros/s/AKfycbyKBD3dFjLjitb4vgLpAhfgGiFzO-f43r5o036s5ke-DF7Ok2NE_04UpR0-7HGdBSKg/exec", true);        
+    data.send();
+    data.onload= function my(){
+    var x = JSON.parse(data.responseText); 
+    var eq= document.getElementsByName("EQUIPMENT_ID")[0].value;
+    var session= document.getElementsByName("MAINTENANCE_TYPE")[0].value;
+    if(session== "MONTHLY") {    
+        document.getElementsByName("DONE_DATE")[0].value=x[11][eq];
+        $("#nextBtn").click();
+        if (x[0][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("ABNORMAL_SOUND_VIBRN_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("ABNORMAL_SOUND_VIBRN_CHECK")[1].checked=true;
+        };
+
+        if (x[1][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("ANY_LEAKAGE_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("ANY_LEAKAGE_CHECK")[1].checked=true;
+        };
+
+        if (x[2][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("DRAIN_BLOCKAGE_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("DRAIN_BLOCKAGE_CHECK")[1].checked=true;
+        };
+
+        if (x[3][eq].toUpperCase()=="DONE") { 
+        document.getElementsByName("AIR_FILTERS_CLEANING_IF_REQR")[0].checked=true;
+        } else{
+        document.getElementsByName("AIR_FILTERS_CLEANING_IF_REQR")[1].checked=true;
+        };
+
+        if (x[4][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("THREE_SPEED_SWITCH_CHECK_AND_REPAIR")[0].checked=true;
+        } else{
+        document.getElementsByName("THREE_SPEED_SWITCH_CHECK_AND_REPAIR")[1].checked=true;
+        };
+
+        if (x[5][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("CHECK_MOTOR_RUNNING_CURRENT_AND_COMPARE")[0].checked=true;
+        } else{
+        document.getElementsByName("CHECK_MOTOR_RUNNING_CURRENT_AND_COMPARE")[1].checked=true;
+        };
+        if (x[6][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("FAN_BELT_TENSION_CHECK_AND_REPAIR")[0].checked=true;
+        } else{
+        document.getElementsByName("FAN_BELT_TENSION_CHECK_AND_REPAIR")[1].checked=true;
+        };
+
+        if (x[7][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("IN_OUT_ISOLATION_BALL_VALVES_CHECK_AND_REPAIR")[0].checked=true;
+        } else{
+        document.getElementsByName("IN_OUT_ISOLATION_BALL_VALVES_CHECK_AND_REPAIR")[1].checked=true;
+        };
+
+        if (x[8][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("MOTOR_FAN_CASING_LOOSENESS_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("MOTOR_FAN_CASING_LOOSENESS_CHECK")[1].checked=true;
+        };
+
+      document.getElementsByName("REMARKS")[0].value=x[9][eq];
+    }
+    else if(session== "WEEKLY"){     
+        document.getElementsByName("DONE_DATE")[0].value=x[10][eq];
+        $("#nextBtn").click();
+        if (x[0][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("ABNORMAL_SOUND_VIBRN_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("ABNORMAL_SOUND_VIBRN_CHECK")[1].checked=true;
+        };
+
+        if (x[1][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("ANY_LEAKAGE_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("ANY_LEAKAGE_CHECK")[1].checked=true;
+        };
+
+        if (x[2][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("DRAIN_BLOCKAGE_CHECK")[0].checked=true;
+        } else{
+        document.getElementsByName("DRAIN_BLOCKAGE_CHECK")[1].checked=true;
+        };
+
+        if (x[3][eq].toUpperCase()=="DONE") { 
+        document.getElementsByName("AIR_FILTERS_CLEANING_IF_REQR")[0].checked=true;
+        } else{
+        document.getElementsByName("AIR_FILTERS_CLEANING_IF_REQR")[1].checked=true;
+        };
+
+        if (x[4][eq].toUpperCase()=="CHECKED") { 
+        document.getElementsByName("THREE_SPEED_SWITCH_CHECK_AND_REPAIR")[0].checked=true;
+        } else{
+        document.getElementsByName("THREE_SPEED_SWITCH_CHECK_AND_REPAIR")[1].checked=true;
+        };
+        document.getElementsByName("REMARKS")[0].value=x[9][eq];
+    }
+    }   
+    };    
+    myf();
+    
